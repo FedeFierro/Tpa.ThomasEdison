ae7bea99276412204c1245e6326ead667668aed9
salguerojonatan <salguerojonatan@Jonatan>
2019-04-27 23:22:57 -0300

    Pared

--- BomberMan/src/entidades/Pared.java
+++ BomberMan/src/entidades/Pared.java
@@ -5,6 +5,11 @@ public class Pared extends Elemento{
 	private int _x;
 	private int _y;
 	
+	public Pared(){
+		this._x = 1;
+		this._y = 1;
+	}
+	
 	public Pared(int x,int y){
 		this._x =x;
 		this._y=y;
