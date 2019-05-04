5e493e4cd2118d497ba0ea9ba95ae497f7760da0
salguerojonatan <salguerojonatan@Jonatan>
2019-05-04 17:26:41 -0300

    +Destrui(), variables

--- BomberMan/src/entidades/Bomba.java
+++ BomberMan/src/entidades/Bomba.java
@@ -1,7 +1,7 @@
 package entidades;
 
 
-public class Bomba extends ElementoDestruible{
+public class Bomba extends Elemento{
 	private Jugador _jugador;
 	protected int _tiempoexplosion;
 	private int rango;
@@ -21,45 +21,45 @@ public class Bomba extends ElementoDestruible{
 		explotaDer();
 		explotaArriba();
 		explotaAbajo();
-		this.destruirse();
+		this.destruir();
 		
 	}//fin explotarBomba
 	private void explotaIzq() {
-		int posFinal = (_x -rango) < 0 ? 0: _x-rango ;
-		for (int i = _x ; i >= posFinal; i--) {
-			if(! puedeContinuar(i, _y)) {
+		int posFinal = (x -rango) < 0 ? 0: x-rango ;
+		for (int i = x ; i >= posFinal; i--) {
+			if(! puedeContinuar(i, y)) {
 				return;	
 			}
 		}
 	}
 	
 	private void explotaDer() {
-		int posFinal = (_x + rango) > _tablero.obtenerAncho() ? _tablero.obtenerAncho(): _x+rango ;
-		for (int i = _x; i <= posFinal; i++) {
-			if(!puedeContinuar(i,_y)) {
+		int posFinal = (x + rango) > tablero.obtenerAncho() ? tablero.obtenerAncho(): x+rango ;
+		for (int i = x; i <= posFinal; i++) {
+			if(!puedeContinuar(i,y)) {
 				return;
 			}
 		}
 	}
 	private void explotaArriba() {
-		int posFinal = (_y - rango) < 0 ? 0: _x-rango ;
-		for (int i = _y; i <= posFinal; i--) {
-			if(!puedeContinuar(_x,i)) {
+		int posFinal = (y - rango) < 0 ? 0: x-rango ;
+		for (int i = y; i <= posFinal; i--) {
+			if(!puedeContinuar(x,i)) {
 				return;
 			}
 		}
 	}
 	private void explotaAbajo() {
-		int posFinal = (_y + rango) > _tablero.obtenerLargo() ? _tablero.obtenerLargo(): _y + rango ;
-		for (int i = _y; i <= posFinal; i++) {
-			if(!puedeContinuar(_x,i)) {
+		int posFinal = (y + rango) > tablero.obtenerLargo() ? tablero.obtenerLargo(): y + rango ;
+		for (int i = y; i <= posFinal; i++) {
+			if(!puedeContinuar(x,i)) {
 				return;
 			}
 		}
 	}
 	
 	private boolean puedeContinuar(int x, int y) {
-		Elemento e = _tablero.obtenerElemento(x,y);
+		Elemento e = tablero.obtenerElemento(x,y);
 		if(e instanceof ElementoDestruible) {
 			if(e instanceof Bomba && e != this) {
 				((Bomba) e).explotar();
@@ -71,5 +71,10 @@ public class Bomba extends ElementoDestruible{
 			return false;
 		}
 		return true;
+	}
+
+	
+	public void destruir() {
+		this.vivo = false;
 	}
 }
