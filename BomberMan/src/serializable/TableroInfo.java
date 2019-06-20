3449c34114f71b9e283dde273d0b96a1cb7e165f
martinezocr <oscar19@live.com.ar>
2019-06-20 16:07:40 -0300

    # WARNING: head commit changed in the meantime
    .

--- BomberMan/src/serializable/TableroInfo.java
+++ BomberMan/src/serializable/TableroInfo.java
@@ -1,9 +1,16 @@
 package serializable;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TableroInfo {
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+public class TableroInfo implements JsonSerializer<TableroInfo>{
 	public int nivel;
 	public String tiempo;
 	public String imagen;
@@ -24,5 +31,20 @@ public class TableroInfo {
 		finJuego = false;
 		jugadoresInfo = new ArrayList<JugadorInfo>();
 	}
+
+	@Override
+	public JsonElement serialize(TableroInfo tableroInfo, Type type, JsonSerializationContext conext) {
+		// TODO Auto-generated method stub
+		JsonObject resultado = new JsonObject();
+		resultado.add("nivel", new JsonPrimitive(nivel));
+		resultado.add("tiempo", new JsonPrimitive(tiempo));
+		resultado.add("imagen", new JsonPrimitive(imagen));
+		resultado.add("pausa", new JsonPrimitive(pausa));
+		resultado.add("puntosPartida", new JsonPrimitive(puntosPartida));
+		resultado.add("ganador", new JsonPrimitive(ganador));
+		resultado.add("finJuego", new JsonPrimitive(finJuego));
+		resultado.add("sonido", new JsonPrimitive(sonido));
+		return resultado;
+	}	
 	
 }
